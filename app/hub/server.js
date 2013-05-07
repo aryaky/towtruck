f1c118478bf4a845dbc687f301b82293b3df2e6d
JackyLee <lilt@channelsoft.com>
2013-05-07 13:12:10 +0800

    Revert "changed for config"
    This reverts commit 2b1e40530a046dbc20560b2bac05e59f1d29bd96.

--- app/hub/server.js
+++ app/hub/server.js
@@ -1,9 +1,8 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
-console.log('test1');
+
 var WebSocketServer = require('websocket').server;
-console.log('test2');
 var WebSocketRouter = require('websocket').router;
 var http = require('http');
 var parseUrl = require('url').parse;
